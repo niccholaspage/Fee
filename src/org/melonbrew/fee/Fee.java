1cede0a0fe80047d6176efc42cef3247fb386815
Nicholas Nassar <niccholaspage@gmail.com>
2012-08-26 20:45:42 -0400

    Add in config

--- src/org/melonbrew/fee/Fee.java
+++ src/org/melonbrew/fee/Fee.java
@@ -10,6 +10,16 @@ public class Fee extends JavaPlugin {
 	
 	public void onEnable(){
 		log = getServer().getLogger();
+		
+		getConfig().options().copyDefaults(true);
+		
+		getConfig().options().header("Fee Config - melonbrew.org\n" +
+				"# serveraccount - An account for fees to go too. (Blank for none)\n" +
+				"# closespeed - How many milliseconds (1000 milliseconds is 1 second) before doors, trapdoors and gates auto close.\n" +
+				"# globalcommands - A command followed by it's cost. For all players.\n" +
+				"# groupcommands - Per group commands.);\n");
+		
+		saveConfig();
 	}
 	
 	public void log(String message){
